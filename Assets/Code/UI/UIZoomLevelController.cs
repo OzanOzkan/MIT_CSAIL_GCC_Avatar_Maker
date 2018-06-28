1d7ddd0d26260ccb8338c195d62e9bec66f83f0e
Ozan Özkan <ozanozkan@gmail.com>
2018-06-28 13:37:36 +0300

    Zoom Y position adjustment.

--- Assets/Code/UI/UIZoomLevelController.cs
+++ Assets/Code/UI/UIZoomLevelController.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class UIZoomLevelController : MonoBehaviour {
 
+    float oldSliderValue;
+
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<Slider>().onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        oldSliderValue = gameObject.GetComponent<Slider>().value;
     }
 	
 	// Update is called once per frame
@@ -20,6 +23,21 @@ public class UIZoomLevelController : MonoBehaviour {
         float sliderValue = gameObject.GetComponent<Slider>().value;
 
         AvatarCreatorContext.faceObject.transform.localScale = new Vector3(sliderValue, sliderValue, 0);
+
+        float test = ((280 - 0) / (gameObject.GetComponent<Slider>().maxValue - gameObject.GetComponent<Slider>().minValue)) * (gameObject.GetComponent<Slider>().maxValue - gameObject.GetComponent<Slider>().value);
+
+
+        //float final = ((sliderValue - 20f) * (280 / 130));
+
+        //if (sliderValue < oldSliderValue)
+        //{
+            AvatarCreatorContext.faceObject.transform.position = new Vector3(AvatarCreatorContext.faceObject.transform.position.x, test, 0);
+      //  }
+
+        //oldSliderValue = sliderValue;
+
+        //  Mathf.Clamp()
+
         //AvatarCreatorContext.faceObject.transform.position = new Vector3(AvatarCreatorContext.faceObject.transform.position.x, AvatarCreatorContext.faceObject.transform.position.y - (sliderValue*2), 0);
     }
 }
