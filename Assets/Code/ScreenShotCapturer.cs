af938fe6122a20dd56e6a4dc36c476c72bf3e033
OzanOzkan <ozanozkan@gmail.com>
2018-07-02 18:32:17 +0300

    Screenshot camera v1.

--- Assets/Code/ScreenShotCapturer.cs
+++ Assets/Code/ScreenShotCapturer.cs
@@ -15,19 +15,29 @@ public class ScreenShotCapturer : MonoBehaviour {
         if (AvatarCreatorContext.takeScreenShot)
         {
             Debug.Log("ScreenShotCapturer:OnPostRender: Capturing screenshot.");
-            var tex = new Texture2D(w, h, TextureFormat.RGB24, false);
+            //var tex = new Texture2D(w, h, TextureFormat.RGB24, false);
 
-            Rect rex = new Rect(x, y, w, h);
-            tex.ReadPixels(rex, 0, 0);
+            //Rect rex = new Rect(x, y, w, h);
+            //tex.ReadPixels(rex, 0, 0);
+            //tex.Apply();
+
+            ////GameObject.Find("testimg").GetComponent<Image>().sprite = Sprite.Create(tex, new Rect(0, 0, rex.width, rex.height), new Vector2(0.5f, 0.5f));
+
+            //AvatarCreatorContext.fileTransferManager.DownloadScreenshot(tex.EncodeToPNG());
+
+            //Destroy(tex);
+
+            //AvatarCreatorContext.takeScreenShot = false;
+
+            RenderTexture originalTexture = RenderTexture.active;
+            RenderTexture ssrTexture = gameObject.GetComponent<Camera>().activeTexture;
+            RenderTexture.active = ssrTexture;
+            Texture2D tex = new Texture2D(ssrTexture.width, ssrTexture.height);
+            tex.ReadPixels(new Rect(0, 0, ssrTexture.width, ssrTexture.height), 0, 0);
             tex.Apply();
+            RenderTexture.active = originalTexture;
 
-            //GameObject.Find("testimg").GetComponent<Image>().sprite = Sprite.Create(tex, new Rect(0, 0, rex.width, rex.height), new Vector2(0.5f, 0.5f));
-
-            AvatarCreatorContext.fileTransferManager.DownloadScreenshot(tex.EncodeToPNG());
-
-            Destroy(tex);
-
-            AvatarCreatorContext.takeScreenShot = false;
+            GameObject.Find("testimg").GetComponent<Image>().sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
         }
     }
 }
